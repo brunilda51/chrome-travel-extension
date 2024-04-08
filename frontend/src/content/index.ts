d8426619858af738e2b7cd97d5e49230073ce269
Brunilda Metaj <b.metaj51@gmail.com>
2024-04-08 02:07:48 +0200

    notifications

--- frontend/src/content/index.ts
+++ frontend/src/content/index.ts
@@ -15,6 +15,28 @@ enum Website {
   KAYAK = 'kayak',
 }
 
+const showNotification = (message: string) => {
+  const notification = document.createElement('div');
+  notification.className = 'extension-notification';
+  notification.textContent = message;
+
+  notification.style.position = 'fixed';
+  notification.style.maxWidth = '200px';
+  notification.style.bottom = '20px';
+  notification.style.right = '20px';
+  notification.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+  notification.style.color = '#fff';
+  notification.style.padding = '10px';
+  notification.style.borderRadius = '5px';
+  notification.style.zIndex = '9999';
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 5000);
+};
+
 if (!window.hasRun) {
   const currentUrl = new URL(window.location.href);
 
@@ -32,33 +54,10 @@ if (!window.hasRun) {
 
     sendMessageToBackground(Website.KAYAK, origin_code, destination_code, departure, arrival);
   }
-  showNotification('Click here to see the sustainability info from your most recent trip search!');
+  showNotification('Click eco mio to see the sustainability info from your trip search!');
   window.hasRun = true;
 }
-function showNotification(message: string) {
-  // Create a div element for the notification
-  const notification = document.createElement('div');
-  notification.className = 'extension-notification';
-  notification.textContent = message;
 
-  // Style the notification (you can adjust styles as needed)
-  notification.style.position = 'fixed';
-  notification.style.bottom = '20px';
-  notification.style.right = '20px';
-  notification.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-  notification.style.color = '#fff';
-  notification.style.padding = '10px';
-  notification.style.borderRadius = '5px';
-  notification.style.zIndex = '9999';
-
-  // Append the notification to the body of the page
-  document.body.appendChild(notification);
-
-  // Automatically remove the notification after a certain time (e.g., 5 seconds)
-  setTimeout(() => {
-    notification.remove();
-  }, 5000); // Adjust the time as needed
-}
 function sendMessageToBackground(
   website: Website,
   origin_code: string,
